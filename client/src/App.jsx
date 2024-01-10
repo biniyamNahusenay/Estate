5ef8e53a7c68f0982fd03cd894ea26b5d6fc10d5
biniyamNahusenay <bininahu12@gmail.com>
2024-01-10 10:10:37 -0800

    react js and tailwind css and other pages and components started

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Header from './components/Header'
+
+export default function App() {
+  return (  
+    <Router>
+       <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </Router>
+  )
+}
