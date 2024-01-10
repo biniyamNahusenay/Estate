fe00b8b28e171ae0e9c4f8a4715988f093675ad6
biniyamNahusenay <bininahu12@gmail.com>
2024-01-10 14:02:03 -0800

    create a signup error

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const env = require("dotenv").config() 
 const mongoose = require("mongoose")
+const userRoutes = require("./routes/userRoutes")
+const authRoutes = require("./routes/authRoutes")
 const cors = require("cors")
 const app = express()
 
 app.use(cors())
 app.use(express.json({limit:"10mb"}))
+app.use("/api/user",userRoutes)
+app.use("/api/auth",authRoutes)
 
 try{
     mongoose.connect(process.env.mongo_url)
