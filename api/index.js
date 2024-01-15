c004db160a1fb76c434a762e9e4b38f57c66b0d8
biniyamNahusenay <bininahu12@gmail.com>
2024-01-15 01:39:46 -0800

    add a redux toolkit

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ try{
 
 app.use((err,req,res,next)=>{
   const statusCode = err.statusCode || 500
-  const message = err.message || "internaserver error"
+  const message = err.message || "internal server error"
   return res.status(statusCode).json({
     success:false,
     statusCode,
