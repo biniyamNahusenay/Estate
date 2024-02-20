fd73d3c2182e2ec49068e4341bb7df5e64477856
biniyamNahusenay <bininahu12@gmail.com>
2024-02-20 08:41:07 -0800

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,15 +1,13 @@
 const express = require("express")
 const env = require("dotenv").config() 
 const mongoose = require("mongoose")
-const userRoutes = require("./routes/userRoutes")
-const authRoutes = require("./routes/authRoutes")
+//const userRoutes = require("./routes/userRoutes")
+//const authRoutes = require("./routes/authRoutes")
 const cors = require("cors")
 const app = express()
 
 app.use(cors())
-app.use(express.json({limit:"10mb"}))
-app.use("/api/user",userRoutes)
-app.use("/api/auth",authRoutes)
+app.use(express.json())
 
 try{
     mongoose.connect(process.env.mongo_url)
@@ -18,16 +16,6 @@ try{
    console.log(err)
    process.exit(1)
 }
-
-app.use((err,req,res,next)=>{
-  const statusCode = err.statusCode || 500
-  const message = err.message || "internal server error"
-  return res.status(statusCode).json({
-    success:false,
-    statusCode,
-    message
-  })
-})
 
 app.listen(3000,()=>{
     console.log("server started")
