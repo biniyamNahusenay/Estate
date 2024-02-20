49b5a8e582aaeaf735b055d1850b217c640a18ac
biniyamNahusenay <bininahu12@gmail.com>
2024-02-20 09:37:11 -0800

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const env = require("dotenv").config() 
 const mongoose = require("mongoose")
-//const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes")
 //const authRoutes = require("./routes/authRoutes")
 const cors = require("cors")
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/api/user", userRoutes)
 
 try{
     mongoose.connect(process.env.mongo_url)
