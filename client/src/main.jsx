adf0daf6b983991c2c7e6d0eecc311eba554cad0
biniyamNahusenay <bininahu12@gmail.com>
2024-02-22 23:32:08 -0800

    complete signup page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
+//import { Provider } from 'react-redux'
 //import { store } from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
 )
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import {Link} from "react-router-dom"
 
-export default function Profile() {
+export default function Signup() {
   return (
-    <div>Sign up</div>
+    <div className = "max-w-lg p-3 mx-auto">
+       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+        <form className="flex flex-col gap-4">
+          <input type="text" placeholder="username" 
+          className="border p-3 rounded-lg"  id="username"/>
+          <input type="email" placeholder="email" id="email"
+          className="border p-3 rounded-lg"/>
+          <input type="password" placeholder="password" id="password"
+           className="border p-3 rounded-lg"/>
+           <button className ="bg-slate-700 p-3 w-100 text-white 
+           rounded-lg hover:opacity-95 disabled:opacity-80 uppercase">Sign Up</button>
+        </form>
+         <div className="flex gap-2 mt-5">
+           <p>Have an account?</p>
+           <Link to="/signin"><span className="text-blue-700">Sign in</span></Link>
+         </div>
+    </div>
   )
 }
